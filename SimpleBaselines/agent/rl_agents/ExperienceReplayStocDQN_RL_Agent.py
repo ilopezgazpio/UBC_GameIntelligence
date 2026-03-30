cdf942a5a724b71998b2a99440d3a709e4d844f7
monre08 <152005743+monre08@users.noreply.github.com>
2026-03-30 17:06:48 +0200

    Create ExperienceReplayStocDQN_RL_Agent.py
    Agente estocástico con Experience Replay, con memoria Naive.

---
+++ SimpleBaselines/agent/rl_agents/ExperienceReplayStocDQN_RL_Agent.py
@@ -0,0 +1,99 @@
+from SimpleBaselines.states.State import State
+from SimpleBaselines.agent.rl_agents.SthocasticDQN_RL_Agent import SthocasticDQN_RL_Agent
+from SimpleBaselines.memory.NaiveExperienceReplay import NaiveExperienceReplay
+
+import gymnasium as gym
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+
+class ExperienceReplayStocDQN_RL_Agent(SthocasticDQN_RL_Agent):
+
+    def __init__(self,
+                 env:gym.Env,
+                 seed=None,
+                 gamma=0.99,
+                 nn_learning_rate=0.01,
+                 egreedy=0.9,
+                 egreedy_final=0.02,
+                 egreedy_decay=500,
+                 hidden_layers_size=[64],
+                 activation_fn=nn.Tanh,
+                 dropout=0.0,
+                 use_batch_norm=False,
+                 loss_fn=nn.MSELoss,
+                 optimizer=optim.Adam,
+                 memory_size=50000,
+                 batch_size=32
+                 ):
+
+        super().__init__(env=env,
+                         seed=seed,
+                         gamma=gamma,
+                         nn_learning_rate=nn_learning_rate,
+                         egreedy=egreedy,
+                         egreedy_final=egreedy_final,
+                         egreedy_decay=egreedy_decay,
+                         hidden_layers_size=hidden_layers_size,
+                         activation_fn=activation_fn,
+                         dropout=dropout,
+                         use_batch_norm=use_batch_norm,
+                         loss_fn=loss_fn,
+                         optimizer=optimizer
+                         )
+
+
+        ''' Parameters for the Experience Replay DQN agent '''
+        self.memory_size = memory_size
+        self.batch_size = batch_size
+
+        # Create memory buffer
+        self.memory = NaiveExperienceReplay(memory_size=self.memory_size, batch_size=self.batch_size)
+
+        # Set action decision function
+        # Experience Replay Stochastic DQN agent plays as a standard Stochastic DQN
+        # internal estimator NN updates following the stochastic scenario Bellman equation under specified loss function
+
+        # action decision function is inherited from superclass
+        # self.__action_decision_function__ = self.__DQN_decision_function__
+
+        # we need to update the update function to use experience replay
+        self.__update_function__ = self.__ER_DQN_bellman_update__
+
+
+    def __ER_DQN_bellman_update__(self, old_state: State, action, new_observation : gym.Space, reward: float, terminated, truncated):
+
+        
+        self.memory.push(old_state.observation, action, new_observation, reward, terminated, truncated)
+                
+        if len(self.memory) < self.batch_size:
+            # not enough samples in memory
+            return
+
+        old_observation_batch, action_batch, new_observation_batch, reward_batch, terminated_batch, truncated_batch = self.memory.sample()
+
+        '''DQN Bellman equation update'''
+        old_observation_batch = self.QNetwork.toDevice(old_observation_batch)
+        action_batch = self.QNetwork.toDevice(action_batch, dType=torch.int64)
+        new_observation_batch = self.QNetwork.toDevice(new_observation_batch)
+        reward_batch = self.QNetwork.toDevice(reward_batch)
+        terminated_batch = self.QNetwork.toDevice(terminated_batch, dType=torch.uint8)
+        truncated_batch = self.QNetwork.toDevice(truncated_batch, dType=torch.uint8)
+
+        # NN is used to predict Q table for a given state --> Q[state, actions]
+        # detach is used to avoid gradients at this time, we want to train NN with state, not with next_observation
+        '''Stochastic Bellman equation update
+        Current memory cell (left part of Stochastic Q-learning equation)'''
+        current_memory = self.QNetwork(old_observation_batch).gather(1, action_batch.unsqueeze(1)).squeeze(1)
+
+        with torch.no_grad():
+
+            '''Input gate cell (right part of Q-learning equation)'''
+            q_next = self.QNetwork(new_observation_batch)
+            input_gate = reward_batch + (1 - (terminated_batch | truncated_batch)) * self.gamma * torch.max(q_next, dim=1)[0]
+
+            '''Bellman equation for stochastic environment'''
+            target_value = (1 - self.Q_learning_rate) * current_memory + self.Q_learning_rate * input_gate
+        
+        self.QNetwork.update_NN(current_memory, target_value)
